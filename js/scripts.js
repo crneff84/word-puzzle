81e39d7182e42e721d7e646a6d01a4bd12952ed9
Chance Neff and Caleb Stevenson <chance@>
2016-08-16 15:53:13 -0700

    add form to index.html, backend and front end to js

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var puzzleSentence = function(sentence) {
+  var letterArray = sentence.split("");
+  for (var i = 0; i < letterArray.length; i +=1) {
+    if (letterArray[i] === "a" || letterArray[i] === "e" || letterArray[i] === "i" || letterArray[i] === "o" || letterArray[i] === "u") {
+      letterArray.splice(i, 1, "-");
+      var puzzledSentence = letterArray.join(" ");
+    }
+  };
+  return puzzledSentence;
+};
+
+
+
+
+
+
+$(document).ready(function(){
+  $("#blanks form").submit(function(event){
+    event.preventDefault();
+    var userInput = $("#sentence").val();
+    var outPut = puzzleSentence(userInput);
+    $("#puzzled-sentence").empty().append(outPut);
+    $("#blanks").hide();
+  });
+});
+
+// var string = "Believe you can and you're halfway there. Theodore Roosevelt"
+// for (var i = 0; i < string.length; i +=1) {
+// console.log(string[i]);
+// }
